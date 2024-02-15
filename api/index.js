ec786972c69de4dfb2a329ba39fa85f31d25d56f
virajsaranga <virajridi@gmail.com>
2024-02-15 11:17:09 +0530

    -state-client side and api initial stage

---
+++ api/index.js
@@ -0,0 +1,8 @@
+import Express  from "express";
+
+const app = Express();
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+}
+);
---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/Signin';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      {/* <Route path='/' element={<Home />} />
+      <Route path='/sign-in' element={<SignIn />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/about' element={<About />} /> */}
+
+
+      
+    </Routes>
+  </BrowserRouter>
+  );
+}
+
+export default App
