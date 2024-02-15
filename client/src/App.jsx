cfe21aed8adf7d97ebc2033fc4bf74c3277ec035
virajsaranga <virajridi@gmail.com>
2024-02-15 20:38:30 +0530

    Sign page initial design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,10 +12,10 @@ function App() {
     <BrowserRouter>
     <Header />
     <Routes>
-      {/* <Route path='/' element={<Home />} />
+       <Route path='/' element={<Home />} />
       <Route path='/sign-in' element={<SignIn />} />
       <Route path='/sign-up' element={<SignUp />} />
-      <Route path='/about' element={<About />} /> */}
+      <Route path='/about' element={<About />} /> 
 
 
       
