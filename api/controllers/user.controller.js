df375a6cddbd19880c226101a2c93ef4f27c1e43
virajsaranga <virajridi@gmail.com>
2024-02-15 20:36:35 +0530

    Backend API connected for user

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,10 @@
+import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
+import Listing from '../models/listing.model.js';
+
+export const test = (req, res) => {
+  res.json({
+    message: 'Api route is working!',
+  });
+};
--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
-import Express  from "express";
-import mongoose from "mongoose";
+import express  from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO) 
@@ -10,8 +13,26 @@ mongoose.connect(process.env.MONGO)
 .catch((err) => {
   console.log(err);
 });
+
+
 const app = Express();
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 }
-)
+)  
+
+app.use('/api/user', userRouter);
+app.use('/api/auth',authRouter);
+ 
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
+  
